9fad01abec4322cf4717adf33a12c42513a447ed
aiX <madmaxdmitrochenko@gmail.com>
2023-05-12 23:55:03 +0300

    negr

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket socket;
+        List<Socket> clients = new List<Socket>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IPEndPoint IPEndPoint = new IPEndPoint(IPAddress.Any, 8888);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(IPEndPoint);
+            socket.Listen(1000);
+
+            ListenToClients();
+
+        }
+        private async Task ListenToClients()
+        {
+            while (true)
+            {
+                var client = await socket.AcceptAsync();
+                clients.Add(client);
+                ReceiveMessage(client);
+            }
+        }
+        private async Task ReceiveMessage(Socket client)
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await client.ReceiveAsync(bytes, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+
+                ListBox.Items.Add($"{client.RemoteEndPoint}: {message}");
+                foreach (var item in clients)
+                {
+                    SendMessage(item, message);
+                }
+            }
+        }
+        private async Task SendMessage(Socket client, string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(bytes, SocketFlags.None);
+        }
+    }
+}
